0d37ea287ca60a5c094e41ad07d6a8a76c81a2ec
c650 <c650@users.noreply.github.com>
2015-06-25 13:30:42 -0400

    Update Calculator.rb

--- Calculator.rb
+++ Calculator.rb
@@ -1,4 +1,38 @@
+def take_input
+  puts "Please input a single-step math operation:"
+  operation_input = gets.chomp
+  
+  if operation_input.include? "*"
+    storage = operation_input.split("*")
+    num1 = storage.first
+    num2 = storage.last
+    mult(num1,num2)
+  elsif operation_input.include? "x"
+    storage = operation_input.split("x")
+    num1 = storage.first
+    num2 = storage.last
+    mult(num1,num2)
+  elsif operation_input.include? "/"
+    storage = operation_input.split("/")
+    num1 = storage.first
+    num2 = storage.last
+    div(num1,num2)
+  elsif operation_input.include? "-"
+    storage = operation_input.split("-")
+    num1 = storage.first
+    num2 = storage.last
+    subt(num1,num2)
+  elsif operation_input.include? "+"
+    storage = operation_input.split("+")
+    num1 = storage.first
+    num2 = storage.last
+    add(num1,num2)
+  else
+    puts "ERROR: Invalid operation request."
+  end
+end
+
 def mult(num1, num2)
   puts (num1 * num2)
 end
-mult(8, 6)
+
