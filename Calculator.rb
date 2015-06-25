aebaed4b86971bb25bb6cbee3a4ce0310480b9bb
c650 <c650@users.noreply.github.com>
2015-06-25 13:44:04 -0400

    Added functionality
    Added functionality for .to_i and solved input error problem

--- Calculator.rb
+++ Calculator.rb
@@ -2,36 +2,45 @@
 def take_input
   puts "Please input a single-step math operation:"
   operation_input = gets.chomp
-  
+  checker = 0 #declare checker variable
+until checker == 1 #loop till correct input is inputted
   if operation_input.include? "*"
     storage = operation_input.split("*")
-    num1 = storage.first
-    num2 = storage.last
+    num1 = storage.first.to_i
+    num2 = storage.last.to_i
+    checker=1
     mult(num1,num2)
   elsif operation_input.include? "x"
     storage = operation_input.split("x")
-    num1 = storage.first
-    num2 = storage.last
+    num1 = storage.first.to_i
+    num2 = storage.last.to_i
+    checker=1
     mult(num1,num2)
   elsif operation_input.include? "/"
     storage = operation_input.split("/")
-    num1 = storage.first
-    num2 = storage.last
+    num1 = storage.first.to_i
+    num2 = storage.last.to_i
+    checker=1
     div(num1,num2)
   elsif operation_input.include? "-"
     storage = operation_input.split("-")
-    num1 = storage.first
-    num2 = storage.last
+    num1 = storage.first.to_i
+    num2 = storage.last.to_i
+    checker=1
     subt(num1,num2)
   elsif operation_input.include? "+"
     storage = operation_input.split("+")
-    num1 = storage.first
-    num2 = storage.last
+    num1 = storage.first.to_i
+    num2 = storage.last.to_i
+    checker=1
     add(num1,num2)
   else
-    puts "ERROR: Invalid operation request."
+    puts "ERROR: Invalid operation request. Enter again:"
+    operation_input = gets.chomp
+    checker=0
   end
-end
+  end #end checker loop
+end 
 #end input control
 def mult(num1, num2)
   puts (num1 * num2)
